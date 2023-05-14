03b5360b69d0da27c4cd0b2a5ab758e553dae946
Abylay <abylay0505@gmail.com>
2023-05-14 21:59:48 +0600

    docs added

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -3,16 +3,20 @@ public class MyHashTable<K, V> {
     private class HashNode<K, V> {
         private K key;
         private V value;
+        //constructor
         public HashNode(K key, V value) {
             this.key = key;
             this.value = value;
         }
+        //returns the key of the hash node
         public K getKey() {
             return key;
         }
+        //returns the value of the hash node
         public V getValue() {
             return value;
         }
+        //sets the value of the hash node
         public void setValue(V value) {
             this.value = value;
         }
@@ -24,12 +28,14 @@ public class MyHashTable<K, V> {
     public LinkedList<HashNode<K, V>>[] chain;
     public int M = 11;
     public int size;
+    // constructor with no param
     public MyHashTable() {
         this.chain = new LinkedList[M];
         for (int i = 0; i < M; i++) {
             chain[i] = new LinkedList<>();
         }
     }
+    // constructor with the size M
     public MyHashTable(int M) {
         this.M = M;
         this.chain = new LinkedList[M];
@@ -37,14 +43,16 @@ public class MyHashTable<K, V> {
             chain[i] = new LinkedList<>();
         }
     }
+    // hash - takes a key and returns an index in the array of linked lists
     private int hash(K key) {
         int hashCode = 0;
-        String strKey = key.toString();
+        String strKey = key.toString(); // convert the key to a string
         for (int i = 0; i < strKey.length(); i++) {
             hashCode += strKey.charAt(i);
         }
-        return hashCode % M;
+        return hashCode % M; // returning the index of array by taking the hash code modulo
     }
+    // put - adds a key value pair to the hash table
     public void put(K key, V value) {
         int index = hash(key);
         LinkedList<HashNode<K, V>> list = chain[index];
@@ -57,6 +65,7 @@ public class MyHashTable<K, V> {
         list.add(new HashNode<K, V>(key, value));
         size++;
     }
+    // returns the value associated with the given key in the hash table
     public V get(K key) {
         int index = hash(key);
         LinkedList<HashNode<K, V>> list = chain[index];
@@ -67,6 +76,7 @@ public class MyHashTable<K, V> {
         }
         return null;
     }
+    // removes the key-value pair with the given key from the hash table
     public V remove(K key) {
         int index = hash(key);
         LinkedList<HashNode<K, V>> list = chain[index];
@@ -80,6 +90,7 @@ public class MyHashTable<K, V> {
         }
         return null;
     }
+    // returns whether the hash table contains the given value
     public boolean contains(V value) {
         for (int i = 0; i < M; i++) {
             LinkedList<HashNode<K, V>> list = chain[i];
@@ -91,6 +102,7 @@ public class MyHashTable<K, V> {
         }
         return false;
     }
+    // returns the key associated with the given value in the hash table
     public K getKey(V value) {
         for (int i = 0; i < M; i++) {
             LinkedList<HashNode<K, V>> list = chain[i];
