fbbbe1d43d6b49577376d9d15fb9b9636d53fd6a
Abylay <abylay0505@gmail.com>
2023-05-14 20:11:19 +0600

    methods added

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -4,19 +4,24 @@ public class MyHashTable<K, V> {
     private class HashNode<K, V> {
         private K key;
         private V value;
+
         public HashNode(K key, V value) {
             this.key = key;
             this.value = value;
         }
+
         public K getKey() {
             return key;
         }
+
         public V getValue() {
             return value;
         }
+
         public void setValue(V value) {
             this.value = value;
         }
+
         @Override
         public String toString() {
             return "{" + key + " " + value + "}";
@@ -25,6 +30,13 @@ public class MyHashTable<K, V> {
     public LinkedList<HashNode<K, V>>[] chain;
     public int M = 11;
     public int size;
+    public MyHashTable() {}
+    public MyHashTable(int M) {}
+    private int hash(K key) {}
+    public void put(K key, V value) {}
+    public V get(K key) {}
+    public V remove(K key) {}
+    public boolean contains(V value) {}
+    public K getKey(V value) {}
 
-    }
 }
